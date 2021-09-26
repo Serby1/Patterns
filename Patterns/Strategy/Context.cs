986cf11896da03026249c7d2737d202ada1e6861
DESKTOP-E215OST\wlser <wlserbin550@gmail.com>
2021-09-27 00:10:18 +0400

    Strategy

---
+++ Patterns/Strategy/Context.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class Context
+    {
+        // Контекст хранит ссылку на один из объектов Стратегии. Контекст не
+        // знает конкретного класса стратегии. Он должен работать со всеми
+        // стратегиями через интерфейс Стратегии.
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+
+        // Обычно Контекст принимает стратегию через конструктор, а также
+        // предоставляет сеттер для её изменения во время выполнения.
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // Обычно Контекст позволяет заменить объект Стратегии во время
+        // выполнения.
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // Вместо того, чтобы самостоятельно реализовывать множественные версии
+        // алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
+        public void DoSomeBusinessLogic()
+        {
+            Console.WriteLine("Context: Sorting data using the strategy (not sure how it'll do it)");
+            var result = this._strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+}
--- Patterns/Strategy/Program.cs
+++ Patterns/Strategy/Program.cs
@@ -6,7 +6,21 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Клиентский код выбирает конкретную стратегию и передаёт её в
+            // контекст. Клиент должен знать о различиях между стратегиями,
+            // чтобы сделать правильный выбор.
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
         }
+    
     }
 }
